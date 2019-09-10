b3e6127bb7919270e2444204bd7956a7dfb06947
jcsoliman <jcssoliman@gmail.com>
2019-09-11 06:59:53 +0800

    first draft

---
+++ Pybank/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,43 @@
+#budget_data.csv 
+#csv file is a dictionary mmm-yyyy,float
+
+##The total number of months included in the dataset.
+
+#--count of periods: return sum of count mmm-yyyy
+#--import then count?
+#--loop through each keym, then sum +=?
+
+#https://stackoverflow.com/questions/3496518/python-using-a-dictionary-to-count-the-items-in-a-list
+
+
+##The net total amount of Profit/Losses over the entire period.
+
+#--return sum of values
+#--loop through each value, then sum +=?
+#--is there a more efficient code?
+
+
+##The average of the changes in Profit/Losses over the entire period.
+
+#--create variable that calculate delta PNL every period
+#--add these delta to dictionary? nest dictionary? convert to nested dict {mmm-yyyy:[PnL:value,Delta:value]}??? 
+#--loop through each delta, then sum +=?
+#--sum / count of periods - 1
+
+
+#The greatest increase in profits (date and amount) over the entire period.
+
+#get max of delta
+#do we want to loop through and do a compare if prev > current?
+#do we want to create a list, get max of list, pull value of that delta? --
+#return string with month, PnL
+
+
+#The greatest decrease in losses (date and amount) over the entire period.
+
+#get min of delta
+#do we want to loop through and do a compare if prev > current?
+#do we want to create a list, get min of list, pull value of that delta? --
+#return string with month, PnL
+
+##Your final script should print the analysis to the terminal and export a text file with the results.
